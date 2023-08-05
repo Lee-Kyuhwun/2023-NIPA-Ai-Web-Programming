16f984c0ee728487fe1a84ee14e363627402b625
이규훈 <wxyz0904@naver.com>
2023-08-06 02:15:01 +0900

    Feat: 7월 21일 과제
    세부 내용
    바닐라 HTML, CSS를 사용할 것
    오늘 배운 html의 list, table, form 등 실제 문서에서 표현되는 세부 내용들을 구현하여 레이아웃에 채울 것
    css를 사용할 때 block element와 inline element를 고려하여 디자인 할 것
    form 태그를 사용할 시에는 form의 입력폼도 채워넣을 것( 데이터를 넘겨서 처리하는 것은 할 필요 없음)
    css의 inline style, 내부 스타일 시트가 아닌 외부 스타일 시트 방식을 사용할 것
    추가되는 이미지 파일, html 파일, css 파일들을 생성하여 같이 프로젝트내에 추가할 것
    css 선택자는 id 선택자가 아닌 class 선택자를 가급적이면 많이 사용할 것
    class 선택자의 선택자 이름은 최대한 자기가 잡은 레이아웃의 맥락에 맞는 이름을 지을 것 (컨벤션은 내용-세부내용 요러한 형식을 가질 것)

---
+++ 7월/31일/과제2.py
@@ -0,0 +1,12 @@
+# 중복 요소를 포함한 파이썬 배열 생성
+original_array = [1, 2, 3, 4, 5, 3, 7, 2, 9, 10]
+
+# 중복 값 제거를 위한 빈 리스트 생성
+unique_array = []
+
+# for in 문을 사용하여 중복 값을 제거하는 코드
+for element in original_array:
+    if element not in unique_array:
+        unique_array.append(element)
+
+print("중복 제거 후 배열:", unique_array)
